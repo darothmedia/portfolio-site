3770638627c4f0460cd934c9421be8f1212fc04e
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-22 22:30:23 -0500

    add google form functionality

--- src/components/contact/contact_form.jsx
+++ src/components/contact/contact_form.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 class ContactForm extends React.Component {
 
+  submit(){
+    window.location = 'rothmedia.org'
+  }
+
   render(){
     return(
       <div className='wrapper' id='formwrap'>
         <h2>How can I help?</h2>
-        <form action="">
+        <div id="ff-compose"></div>
+        <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeXjPkowWIn-gjxU_U8dcpNVQwKxyUYBFyp9bI-TE48yS8Q2Q/formResponse"
+        onSubmit={this.submit}>
           <div id="inputwrap">
             <label>Your Email</label>
-            <input type="text" />
+            <input type="text" name='entry.358076833' />
           </div>
           <div id='inputwrap'>
             <label>Message</label>
-            <textarea />
+            <textarea name='entry.1738447102' />
           </div>
           
           <button>Submit</button>
