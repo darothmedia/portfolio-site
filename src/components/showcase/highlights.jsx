561ff3d9eb98aaae4185ca5d933f215ee154f617
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-27 14:37:26 -0500

    update styling sitewide

--- src/components/showcase/highlights.jsx
+++ src/components/showcase/highlights.jsx
@@ -3,7 +3,7 @@ const highlights = [
     title: 'Do Not Touch',
     img: 'https://cdn.entries.clios.com/styles/clio_aotw_ems_image_details_retina/s3/entry_attachments/image/51/1422/36804/76289/D9a4N9qK5-_sPbgg2ep-F1pS_smKHy40tjOAtMQDzeI.jpg/D9a4N9qK5-_sPbgg2ep-F1pS_smKHy40tjOAtMQDzeI.jpg',
     url: 'https://apps.apple.com/us/app/do-not-touch-by-nickelodeon/id1355139969',
-    description: 'Product Manager and Lead Producer on the Do Not Touch app, an augmented reality variety show of zany mini-games featuring Nickelodeon characters. Managed the roadmap for new features, pitched creative for new content, and maintained budget and schedule during an international, multi-platform expansion. Winner of the 2019 Kidscreen Award for Best Game App: Branded and the 2018 & 2019 Clio Entertainment Award for Mobile AR.'
+    description: 'Product Manager and Lead Producer on the Do Not Touch app, an augmented reality variety show of mini-games featuring Nickelodeon characters. Managed roadmaps, pitched creative, and maintained budget and schedule during an international, multi-platform expansion. Winner of a 2019 Kidscreen Award and two Clio Entertainment Awards (2018 & 2019) for Mobile AR.'
   },
   {
     title: 'Watch Your Words',
