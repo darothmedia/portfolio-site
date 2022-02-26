a8a144e29cc6b8e4fa6fa922eb79a7b209fff36a
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-25 17:36:39 -0800

    add rocket

--- src/components/main/splash.jsx
+++ src/components/main/splash.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { SplashShowcase } from "../showcase/showcase"
 import About from "../about/about"
+import rocket from '../../rocket.png'
 
 const Splash = props => (
   <div className='wrapper' id='splashwrap'>
@@ -9,6 +10,9 @@ const Splash = props => (
       <h1>Adrian Rothschild</h1>
       <h2>web & app development / product / design</h2>
     </section>
+    <div className="animwrapper">
+      <img src={rocket} className="rocket" alt="rocket" />
+    </div>
     <About />
   </div>
 )
