43d5ad06368890511aa15564cf8e2b327733e77d
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-25 18:34:38 -0800

    rocket placement

--- src/components/main/splash.jsx
+++ src/components/main/splash.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import About from "../about/about"
 import rocket from '../../rocket.png'
-import {Link, animateScroll, Element} from "react-scroll"
+import {Link, Element} from "react-scroll"
 
 const Splash = props => (
   <div className='wrapper' id='splashwrap'>
