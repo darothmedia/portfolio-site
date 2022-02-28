f881b146cf1e3388ced5aedf546681ac8392cf85
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-28 11:50:57 -0800

    remove rocket

--- src/components/main/splash.jsx
+++ src/components/main/splash.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 import About from "../about/about"
-import rocket from '../../rocket.png'
-import {Link, Element} from "react-scroll"
 
 const Splash = props => (
   <div className='wrapper' id='splashwrap'>
@@ -10,15 +8,8 @@ const Splash = props => (
       <h1>Adrian Rothschild</h1>
       <h2>web & app development / product / design</h2>
     </section>
-    <div className="animwrapper">
-      <Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500}>
-        <img src={rocket} className="rocket" alt="rocket" />
-      </Link>
-    </div>
     <div className="page-wrapper" id="pagewrapper">
-      <Element name="about" className="element">
         <About />
-      </Element>
     </div>
   </div>
 )
