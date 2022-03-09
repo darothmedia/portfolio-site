10b720c8b302961fe6246c3c1c7a41cd72ff366f
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-03-09 11:29:17 -0800

    modify recent showcase

--- src/components/showcase/recents.jsx
+++ src/components/showcase/recents.jsx
@@ -1,5 +1,11 @@
 const recents = [
   {
+    title: 'CAPIDLE',
+    img: 'https://live.staticflickr.com/65535/51924752541_d989e5f747_o.png',
+    url: 'https://darothmedia.github.io/capidle/',
+    description: 'A geographic riff on Wordle. Narrow down possible cities by entering city names, then view their distance to the target city. Keep guessing until you succeed! Frontend built in React.js / Redux. Using GeoDB Cities API & Material UI.'
+  },
+  {
     title: 'What the GIF!?',
     img: 'https://i.imgur.com/Ecb2avz.png',
     url: 'https://what-the-gif.herokuapp.com/#/',
@@ -10,12 +16,6 @@ const recents = [
     img: 'https://live.staticflickr.com/65535/51749485128_c0913c0bb9_k.jpg',
     url: 'https://hikers-guide.herokuapp.com',
     description: 'HikersGuide is a playful clone of AllTrails where users can log in, leave reviews of a trail, and view park / trail detail pages with helpful info. The site draws references from The Hitchhikers Guide to the Galaxy, and has several easter eggs that fans of the book will appreciate.'
-  },
-  {
-    title: 'CAPIDLE',
-    img: 'https://live.staticflickr.com/65535/51924752541_d989e5f747_o.png',
-    url: 'https://darothmedia.github.io/capidle/',
-    description: 'A geographic riff on Wordle. Narrow down possible cities by entering city names, then view their distance to the target city. Keep guessing until you succeed! Frontend built in React.js / Redux. Using GeoDB Cities API & Material UI.'
   },
   {
     title: 'Plant Palace',
